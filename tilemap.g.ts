3dcf40ee8fbc160929caed5b0b1e177065a40d3c
lilyn-period4 <82903371+lilyn-period4@users.noreply.github.com>
2021-05-06 13:54:59 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`280010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004000004000000000000000000000000000000000000120e0e000000000000000000000000000000000000000000000000000000000000000000000000001300000000010203000000000102020203000000000000050606220900000000000000120f0f10001300000000000000000000000000000000000000000000070b0b000a00000000000000131615110013001b1c00000000000000000000000000202121212300070b0d000a0000010202030013181711001300000000000000001d1e1f1f00000000000000000000080c000000000000000000221419191a22140000002200000000`, img`
+........................................
+........................................
+........................................
+........2222222222......................
+........2........2......................
+......222........2......................
+.....22..........22.....................
+222222............2222222222222222222...
+...................................22222
+.......................................2
+.......................................2
+................................2......2
+................................2....222
+.................2.........22...2...2222
+.................2.........22...222.2222
+.....................2222..22...222.2222
+`, [myTiles.transparency16,sprites.builtin.forestTiles17,sprites.builtin.forestTiles18,sprites.builtin.forestTiles19,sprites.builtin.forestTiles8,sprites.builtin.forestTiles5,sprites.builtin.forestTiles6,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.builtin.forestTiles7,sprites.builtin.forestTiles11,sprites.builtin.forestTiles10,sprites.dungeon.chestClosed,sprites.builtin.forestTiles14,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleOuterSouthWest,sprites.builtin.forestTiles24,sprites.builtin.forestTiles29,sprites.swamp.swampTile0,sprites.swamp.swampTile1,sprites.swamp.swampTile2,sprites.builtin.forestTiles1,sprites.builtin.forestTiles2,sprites.builtin.forestTiles4,sprites.builtin.forestTiles3], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
